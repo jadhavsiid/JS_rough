cb8555ce64bc7dcd0c0f4268df8a70ecae83d2c5
Jadhav <siddhesh.a.jadhav@accenture.com>
2026-04-15 13:07:41 +0530

    Updated dates

--- Lec-3_prac/Date-&-time-in-js/Date-and-time.js
+++ Lec-3_prac/Date-&-time-in-js/Date-and-time.js
@@ -1,18 +1,54 @@
-let myDate = new Date ()
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
+"use strict"
+// date in js
 
+let myDate = new Date()
+console.log(myDate);
 console.log(typeof myDate);
 
-let mycreatedDate = new Date(2025, 7, 17, 17, 3)
-console.log(mycreatedDate);
-// When you declare date in form of an Array like above your starting month is 0 (Jan)
-let setDate = new Date("2026-08-17")
-console.log(setDate);
-// When you declate date in form of "yyyy-mm-dd" your starting month is 1 (Jan)
+// Converting date to strings
+console.log(myDate.toString());
+console.log(typeof myDate.toString());
 
-let indDate = new Date ("08-17-2027")
-console.log(indDate);
+// converting to toDateString()
+console.log(myDate.toDateString());
+
+// converting to toISOSString()
+console.log(myDate.toISOString());
+
+// converting to toJSON()
+console.log(myDate.toJSON());
+
+// converting to toLocaleString()
+console.log(myDate.toLocaleString());
+
+// converting to toLocaleDateString()
+console.log(myDate.toLocaleDateString());
+
+// converting to toLocaleTimeString()
+console.log(myDate.toLocaleTimeString());
+
+
+// let myCreatedDate = new Date(2026, 0, 1)
+// let myCreatedDate = new Date(2026, 7, 17, 21, 30)
+// let myCreatedDate = new Date(2026, 7, 17, 21, 30)
+// let myCreatedDate = new Date("2027-02-05")
+let myCreatedDate = new Date("04-15-2026")
+console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp); // values in mili seconds
+
+console.log(myCreatedDate.getTime());
+console.log(Math.round(Date.now()/1000));
+
+// ------------------------------------------------------------------------------------
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1 );
+console.log(newDate.getDay());
+
+// newDate.toLocaleString('default', {
+//     weekday: "long"
+// }) read toLocaleString() in depth
 
