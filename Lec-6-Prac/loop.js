b6a87e2b4698b0b3fe37bbe19ae275be7f6a3efa
Jadhav <siddhesh.a.jadhav@accenture.com>
2026-05-02 20:51:39 +0530

    loop-updt

--- Lec-6-Prac/loop.js
+++ Lec-6-Prac/loop.js
@@ -1,14 +1,11 @@
 /* Write a 'while' loop that calculates the sum of all numbers from 1 to 5 and stores the result in a variable named 'sum'. */
 
-let initialVar = 1
-let sum = 0
-while (initialVar <= 5) {
-    sum += initialVar
-    initialVar ++
+let initVar = 1 // 
+let sum = 0 // 
+while (initVar <= 5) {
+    sum += initVar
+    initVar++
 }
 console.log(sum);
 
 /* Write a 'while' loop that counts down from 5 to 1 and stores the numbers in an array named 'countdown' */
-
-let iterableVar = 5
-let collection = 0
